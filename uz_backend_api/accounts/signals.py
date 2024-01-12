e851c0e447b7385f221b25202135c1db4bf39b9e
Jeremiah Muchazondida <jjmuchazondida@gmail.com>
2024-01-12 06:37:05 +0200

    Adding auth code in accounts app

---
+++ uz_backend_api/accounts/signals.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .username import get_username
+from accounts.models import User
+
+
+@receiver(post_save, sender=User)
+def user_save_handler(sender, instance, created, **kwargs):
+    if created:
+        if instance.institution_id is None:
+            instance.username = get_username()
+            instance.save()
+        else:
+            instance.username = get_username(
+                institution=instance.institution_id
+            )
+            instance.save()
+
+# @receiver(post_save, sender=User)
+# def update_username(sender, instance, created, **kwargs):
+#     if created:
+#         # Generate the initial username on creation
+#         instance.username = get_username(instance.institution_code)
+#         instance.save()
